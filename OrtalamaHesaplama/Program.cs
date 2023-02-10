5d621629ab619e70a3f74b91d1a23a5db371c913
Coders Club <mehmetbakmaz6@gmail.com>
2023-02-10 16:12:16 +0300

    Add project files.

---
+++ OrtalamaHesaplama/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrtalamaHesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ogrenci ogrenci = new ogrenci();
+            string name, surname;
+            double not1, not2, not3;
+            
+
+            Console.WriteLine("Ortalama hesaplama uygulamasına hoş geldiniz");
+            Console.WriteLine("Girmek istediğini öğrencilerin bilgilerini yazın");
+            Console.Write("Ad : ");
+            name = Console.ReadLine();
+            Console.Write("Soyad : ");
+            surname = Console.ReadLine();
+            Console.Write("Not1 : ");
+            not1 =Convert.ToDouble(Console.ReadLine());
+            Console.Write("Not2 : ");
+            not2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Not3 : ");
+            not3 = Convert.ToDouble(Console.ReadLine());
+            ogrenci.OrtalamaHesapla(name,surname,not1,not2,not3);
+           
+            Console.Read();
+        }
+    }
+}
---
+++ OrtalamaHesaplama/ogrenci.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrtalamaHesaplama
+{
+    internal class ogrenci
+    {
+        public void OrtalamaHesapla(string ad,string soyad,double not1,double not2,double not3)
+        {
+            Console.WriteLine(ad+" "+soyad+ " adlı kişinin not ortalaması bilgileri aşağıdaki gibidir ");
+            double ort = ortalama(not1, not2, not3);
+            ortalamaDegerlendir(ort);
+            //ortalamaDegerlendir(ortalama(not1,not2,not3));
+        }
+        void ortalamaDegerlendir(double ortalama) 
+        {
+            if (ortalama < 45)
+            {
+                Console.WriteLine("Ortalama değeriniz {0} : Maalesef kaldınız", ortalama);
+            }
+            else
+            {
+                Console.WriteLine("Ortalama değeriniz {0} : Tebrikler geçtiniz", ortalama);
+            }
+        }
+        private double ortalama(double ogrencinot1, double ogrencinot2, double ogrencinot3) 
+        {
+            double ort = (ogrencinot1 + ogrencinot2 + ogrencinot3) / 3;
+            return ort;
+        }
+    }
+}
